8764a14c19e7c0abf0fd823dcff75b565d8e9c1d
Courtney Jones <courtneyrjones95@gmail.com>
2020-08-11 14:37:09 -0500

    favicon

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/db.js", "/index.js", "/manifest.webmanifest", "/styles.css", "/icons/icon-192x192.png", "/icons/icon-512x512.png", "favicon.ico"];
+const FILES_TO_CACHE = ["/", "/index.html", "/db.js", "/index.js", "/manifest.webmanifest", "/styles.css", "/icons/icon-192x192.png", "/icons/icon-512x512.png", "/favicon.ico"];
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
