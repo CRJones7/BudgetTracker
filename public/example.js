c907671a17b617b2cacf6f2dcf41a11b4843c380
Courtney Jones <courtneyrjones95@gmail.com>
2020-08-11 09:52:09 -0500

    fixes

--- public/example.js
+++
@@ -1,4 +0,0 @@
-function example() {
-    console.log("Hello")
-}
-var one; 
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "index.html", "db.js", "index.js", "manifest.webmanifest",];
+const FILES_TO_CACHE = ["/", "/index.html", "/db.js", "/index.js", "/manifest.webmanifest", "/styles.css", "/icons/icon-192x192.png", "/icons/icon-512x512.png"];
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -62,4 +62,11 @@ self.addEventListener("fetch", function (evt) {
 
         return;
     }
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
 });
